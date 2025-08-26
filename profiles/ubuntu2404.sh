58f8896b4a7d7d4f38cc48173cb5a06e0fa97420
Domenic Rodriguez <domenic.rodriguez@geckorobotics.com>
2025-08-26 13:35:08 -0400

    Add starship

--- profiles/ubuntu2404.sh
+++ profiles/ubuntu2404.sh
@@ -8,7 +8,7 @@ sudo apt-get update
 sudo apt-get install -y --no-install-recommends software-properties-common
 sudo add-apt-repository -y ppa:fish-shell/release-4
 
-# Install packages
+# Install APT packages
 sudo apt-get update
 sudo apt-get install -y --no-install-recommends \
     bat \
@@ -17,12 +17,19 @@ sudo apt-get install -y --no-install-recommends \
     fzf \
     jq \
     ripgrep \
+    tmux \
     tree
 
-# Add symlinks
-sudo ln -sf "$(which batcat)" /usr/local/bin/cat
-sudo ln -sf "$(which fdfind)" /usr/local/bin/fd
+# Install Starship
+curl -sS https://starship.rs/install.sh | sudo sh -s -- --yes
 
 # Add shell hooks
 mkdir -p ~/.config/fish
-echo "direnv hook fish | source" >>~/.config/fish/config.fish
+cat <<EOF >>~/.config/fish/config.fish
+direnv hook fish | source
+starship init fish | source
+EOF
+
+# Add symlinks
+sudo ln -sf "$(which batcat)" /usr/local/bin/bat
+sudo ln -sf "$(which fdfind)" /usr/local/bin/fd
